229100f7976ca06a1a6f65c4e8c9328a931e4c3a
xysong <xysong25@gmail.com>
2023-04-17 20:57:05 -0400

    Update: testing code

--- test/test_uno.py
+++ test/test_uno.py
@@ -2,12 +2,15 @@ import pickle
 import torch
 import numpy as np
 from icecream import ic
+import json
 
 # All of imports here are CUSTOMIZED. We are not using RLCards anymore.
 from uno.envs.uno import UnoEnv
 from uno.agents.random_agent import RandomAgent
 
 np.random.seed(2023)
+
+
 DEFAULT_GAME_CONFIG = {
     'game_num_players': 2,
 }
@@ -26,22 +29,22 @@ ic(len(trajectories))
 ic(payoffs)
 
 # Check returned trajectories
-# ic(trajectories[0])
-ic(type(trajectories[0]))
-ic(len(trajectories[0]))
-ic(len(trajectories[1]))
-ic(type(trajectories[0][0]))
-
-
 # Just to get a sense of how "trajectories" are stored
-for idx, item in enumerate(trajectories[1]):
+for idx, item in enumerate(trajectories[0]):
     ic(f"{idx} -- {type(item)}")
     if type(item) == np.int64:
         ic(item)
     else:
         ic(len(item['action_record']))
 
+# Check action recorder functionality
+ic(trajectories[0][0]['action_record'])
+ic(trajectories[0][28]['action_record'])
+assert trajectories[0][28]['action_record'] == trajectories[0][0]['action_record']
 
 # Save trajectories to .json for testing purpose
-# with open("log/test.json", "wb") as f:
-#     pickle.dump(trajectories[0], f)
+with open("log/test.json", "w") as f:
+    trajectories[0][0]['obs'] = trajectories[0][0]['obs'].tolist()
+    for item, val in trajectories[0][0].items():
+        ic(f"{item} -- {type(val)}")
+    json.dump(dict(trajectories[0][0]), f, separators=(',', ': '))

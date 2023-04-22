a03e8f5e91bb6db259a5985d4dde0011c64cbbde
xysong <xysong25@gmail.com>
2023-04-21 22:53:13 -0400

    Update: reformat sarsa_agent.py

--- uno/agents/sarsa_agent.py
+++ uno/agents/sarsa_agent.py
@@ -1,30 +1,30 @@
 import numpy as np
+import torch
+import torch.nn as nn
 from icecream import ic
 
 
 # TODO:(Xiaoyang) REFACTOR THIS CLASS SOON...
 # Note: if any of you want to modify this one, please lmk first.
-class Agent(object):
+class SARSAAgent(object):
 
     def __init__(self, num_actions):
         self.use_raw = False
         self.num_actions = num_actions
-        # Values
-        self.Q = None
+        # Q-Value estimation network: currently the architecture is borrowed from Stanford report
+        self.Q = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(4 * 4 * 15, 512),
+            nn.ReLU(),
+            nn.Linear(512, 61)
+        )
 
     def step(self, state):
         legal_actions = list(state['legal_actions'].keys())
         return np.random.choice(legal_actions)
 
     def eval_step(self, state):
-        ''' Predict the action given the current state for evaluation.
-            Since the random agents are not trained. This function is equivalent to step function
-        Args:
-            state (dict): An dictionary that represents the current state
-        Returns:
-            action (int): The action predicted (randomly chosen) by the random agent
-            probs (list): The list of action probabilities
-        '''
+        # Return optimal policy based on LEARNED policy
         probs = [0 for _ in range(self.num_actions)]
         for i in state['legal_actions']:
             probs[i] = 1/len(state['legal_actions'])
@@ -34,3 +34,13 @@ class Agent(object):
             state['legal_actions'].keys())[i]] for i in range(len(state['legal_actions']))}
 
         return self.step(state), info
+
+
+# Simple Test Code
+
+if __name__ == '__main__':
+    sarsa = SARSAAgent(61)
+    state = torch.zeros((4, 4, 15))
+    # Test nn
+    out = sarsa.Q(state)
+    ic(out.shape)

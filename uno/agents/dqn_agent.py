be0dcfd49179f162251e713f64236059901a851f
“Savannah918” <SavannahTan918@outlook.com>
2023-04-25 14:03:08 -0400

    eval_step method added in dqn_agent

--- uno/agents/dqn_agent.py
+++ uno/agents/dqn_agent.py
@@ -4,13 +4,13 @@ import numpy as np
 from uno.envs.unoenv import UnoEnv
 from uno.agents.random_agent import RandomAgent
 from torch import nn
-from collections import deque
-import random
 
-
+from model.dqn_backbone import QNetwork, ReplayMemory
+from icecream import ic
 
 class DQNAgent:
     def __init__(self, num_actions, state_size=240, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), gamma=0.99, learning_rate=0.001, batch_size=64, memory_size=10000, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.995, update_frequency=1000):
+        self.name = "DQN Agent"
         self.state_size = state_size
         self.use_raw = False
         self.num_actions = num_actions
@@ -36,9 +36,7 @@ class DQNAgent:
         # Reshape the state to the expected input size
         reshaped_state = state['obs'].reshape(-1)
 
-        # if reshaped_state.size != self.state_size:
-        #     reshaped_state = reshaped_state[:self.state_size] if reshaped_state.size > self.state_size else np.pad(
-        #         reshaped_state, (0, self.state_size - reshaped_state.size))
+        legal_actions = state['legal_actions']
 
         if np.random.rand() > self.epsilon:
             reshaped_state = torch.from_numpy(reshaped_state).float().unsqueeze(0).to(self.device)
@@ -46,10 +44,12 @@ class DQNAgent:
             with torch.no_grad():
                 action_values = self.q_network(reshaped_state)
             self.q_network.train()
-            return np.argmax(action_values.cpu().data.numpy())
+            # Select the action with the highest Q-value from legal actions
+            action = max(legal_actions.keys(), key=lambda x: action_values.cpu().data.numpy()[0][x])
         else:
-            return np.random.randint(self.num_actions)
+            action = np.random.choice(list(legal_actions.keys()))
 
+        return action
 
     def store(self, state, action, reward, next_state, done):
         self.memory.store((state, action, reward, next_state, done))
@@ -61,10 +61,17 @@ class DQNAgent:
         experiences = self.memory.sample(self.batch_size)
         states, actions, rewards, next_states, dones = zip(*experiences)
 
-        states = torch.from_numpy(np.vstack(states)).float().to(self.device)
+        states_obs = [state['obs'] for state in states]
+        states = np.vstack(states_obs).reshape(self.batch_size, -1).astype(np.float32)
+        states = torch.from_numpy(states).float().to(self.device)
+
         actions = torch.from_numpy(np.vstack(actions)).long().to(self.device)
         rewards = torch.from_numpy(np.vstack(rewards)).float().to(self.device)
-        next_states = torch.from_numpy(np.vstack(next_states)).float().to(self.device)
+
+        next_states_obs = [state['obs'] for state in next_states]
+        next_states = np.vstack(next_states_obs).reshape(self.batch_size, -1).astype(np.float32)
+        next_states = torch.from_numpy(next_states).float().to(self.device)
+
         dones = torch.from_numpy(np.vstack(dones).astype(np.uint8)).float().to(self.device)
 
         # learn phase
@@ -81,40 +88,15 @@ class DQNAgent:
     def update_target_network(self):
         self.target_network.load_state_dict(self.q_network.state_dict())
 
+    def eval_step(self, state):
+        state_obs = state['obs']
+        state = torch.from_numpy(state_obs).float().unsqueeze(0).to(self.device)
 
+        self.q_network.eval()
+        with torch.no_grad():
+            action_values = self.q_network(state)
+        self.q_network.train()
 
-class QNetwork(nn.Module):
-    def __init__(self, num_actions=61, state_size=240, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), layers=2):
-        super(QNetwork, self).__init__()
-        self.num_actions = num_actions
-        self.state_size = state_size
-        self.layers = layers
-        self.device = device
-
-        layer_dims = [self.state_size] + [128] * self.layers
-        fc = []
-        for i in range(len(layer_dims) - 1):
-            fc.append(nn.Linear(layer_dims[i], layer_dims[i+1], bias=True))
-            fc.append(nn.ReLU())
-        fc.append(nn.Linear(layer_dims[-1], self.num_actions, bias=True))
-        self.fc_layers = nn.Sequential(*fc)
-
-    def forward(self, s):
-        return self.fc_layers(s)
-
-
-class ReplayMemory:
-    def __init__(self, capacity=10000):
-        self.capacity = capacity
-        self.memory = deque(maxlen=capacity)
-
-    def store(self, experience):
-        self.memory.append(experience)
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
+        return np.argmax(action_values.cpu().data.numpy()), None
 
 

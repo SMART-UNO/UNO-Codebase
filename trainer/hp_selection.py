30ca0d1292fa1bab1ddfe003203942b1e3fbc89a
xysong <xysong25@gmail.com>
2023-05-02 01:37:19 -0400

    Update: results

---
+++ trainer/hp_selection.py
@@ -0,0 +1,114 @@
+import numpy as np
+import torch
+from tqdm import tqdm
+from icecream import ic
+# External import
+from uno.envs.unoenv import UnoEnv
+from uno.envs.uno2penv import UnoEnv2P
+from model.sarsa_backbone import SARSA_Q
+from uno.agents.random_agent import RandomAgent
+from uno.agents.sarsa_agent import SARSAAgent
+from uno.agents.mc_agent import MCAgent
+from uno.agents.reinforce_agent import ReinforceAgent
+from uno.agents.dqn_agent import DQNAgent
+from utils import parse_payoffs, DEVICE
+from tests.eval import test_trained_agents, plot_avg_rewards
+
+
+torch.manual_seed(2023)
+np.random.seed(2023)
+LR = [1e-2, 1e-3, 1e-4]
+EPSILON = [0.05, 0.10]
+DISCOUNT = [0.95, 0.99]
+
+# -------------------- Hyperparameter Declaration -------------------- #
+num_episodes = 10000
+T = 10000  # Just some large number
+
+for lr in LR:
+    for eps in EPSILON:
+        for discount_factor in DISCOUNT:
+
+            # ------------------------ Agent Declaration ------------------------ #
+            base_agent = RandomAgent(61)
+
+            # mc_agent = MCAgent(num_actions=61, env=env,
+            #                    lr=lr, eps=eps, df=discount_factor)
+            sarsa_agent = SARSAAgent(
+                num_actions=61, lr=lr, eps=eps, df=discount_factor)
+
+            # --------------------- Environment Declaration --------------------- #
+            env = UnoEnv2P(base_agent, sarsa_agent)
+
+            # ----------------------- Checkpoint Loading ----------------------- #
+            # Load checkpoint if necessary
+            # checkpoint = "checkpoint/SARSA/sarsa-agent-[50000]-[0.0001]-[0.05]-[0.95].pt"
+            # checkpoint = "checkpoint/SARSA/sarsa-agent-[50000].pt"
+            checkpoint = None
+            if checkpoint is not None:
+                sarsa_agent = torch.load(checkpoint,
+                                         map_location=DEVICE)
+                ic('Checkpoint Loaded!')
+                sarsa_agent.Q = sarsa_agent.Q.to(DEVICE)
+
+            # --------------------- Statistics --------------------- #
+            eval_every_n = 1000
+            avg_payoff_sarsa_first, avg_payoff_sarsa_second = [], []
+
+            # --------------------- Training Loop --------------------- #
+            for episode in tqdm(range(num_episodes)):
+                sarsa_agent.Q.train()
+                env.reset()
+                t = 0
+                # Initialize S & A
+                S = env.cur_state
+                A = sarsa_agent.step(S)
+                # Handle base case where the game is over directly
+                if env.is_over():
+                    continue
+
+                # --------------------- Training within one episode --------------------- #
+                while t < T:
+                    t += 1
+                    S_NEW, is_over = env.step(A)
+                    R = env.get_payoffs()[1]
+                    # ic(R)
+                    A_NEW = sarsa_agent.update(S, A, S_NEW, R, is_over)
+                    # Update
+                    A = A_NEW
+                    S = S_NEW
+                    if is_over:
+                        break
+
+                # --------------------- Update Epsilon --------------------- #
+                # if (episode + 1) % update_eps_every_n == 0:
+                #     sarsa_agent.update_eps(decay_rate)
+
+                # --------------------- Evaluation every n episodes --------------------- #
+                if (episode + 1) % eval_every_n == 0:
+                    r_sarsa_first, _ = test_trained_agents(
+                        sarsa_agent, base_agent, 1000, True)
+                    _, r_sarsa_second = test_trained_agents(
+                        base_agent, sarsa_agent, 1000, True)
+                    avg_payoff_sarsa_first.append((episode, r_sarsa_first))
+                    avg_payoff_sarsa_second.append((episode, r_sarsa_second))
+                    # Store back to agents
+                    sarsa_agent.eval_first.append((episode, r_sarsa_first))
+                    sarsa_agent.eval_first.append((episode, r_sarsa_second))
+
+            # --------------------- Save Training Checkpoint --------------------- #
+            torch.save(sarsa_agent,
+                       f"checkpoint/SARSA/sarsa-agent-large-[{num_episodes}]-[{lr}]-[{eps}]-[{discount_factor}].pt")
+
+            # --------------------- Final Evaluation --------------------- #
+            test_trained_agents(sarsa_agent, base_agent, 10000, True)
+            test_trained_agents(base_agent, sarsa_agent, 10000, True)
+
+            # --------------------- Plot Results --------------------- #
+            plt_path = f"log/SARSA/sarsa-agent-[{num_episodes}]-[{lr}]-[{eps}]-[{discount_factor}]"
+            plot_avg_rewards(avg_payoff_sarsa_first,
+                             "Average Rewards (SARSA Agent Plays First)",
+                             plt_path + "-[first].png")
+            plot_avg_rewards(avg_payoff_sarsa_second,
+                             "Average Rewards (SARSA Agent Plays Second)",
+                             plt_path + "-[second].png")
